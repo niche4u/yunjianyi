12aad5e13e4b3180955a3e8eb83c4a53af48b869
chonder <wuchonder@gmail.com>
2015-07-10 19:56:53 +0800

    后台主题正文管理的页面样式调整更好看点

--- backend/views/topic-content/create.php
+++ backend/views/topic-content/create.php
@@ -12,8 +12,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="topic-content-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
